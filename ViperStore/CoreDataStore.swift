c64b35bd27c23e74de595783c81836874037ad89
Varun Rathi <varunrathi28@gmail.com>
2019-07-19 01:11:18 +0530

    Data Store with Plist added

---
+++ ViperStore/CoreDataStore.swift
@@ -0,0 +1,14 @@
+//
+//  CoreDataStore.swift
+//  ViperStore
+//
+//  Created by Varun Rathi on 19/07/19.
+//  Copyright © 2019 Varun Rathi. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CoreDataStore:NSObject {
+
+}
