4a7e728eceae654e71f6c4293ac5faeeb8e2f89e
Varun Rathi <varunrathi28@gmail.com>
2019-07-19 01:31:33 +0530

    1. CoreDataStore Initializer

--- ViperStore/Classes/Data Store/CoreDataStore.swift
+++ ViperStore/Classes/Data Store/CoreDataStore.swift
@@ -10,5 +10,30 @@ import UIKit
 import CoreData
 
 class CoreDataStore:NSObject {
-
+    static let sharedInstance  = CoreDataStore()
+    var persistentStoreCoordinator : NSPersistentStoreCoordinator!
+    var managedObjectModel : NSManagedObjectModel!
+    var managedObjectContext : NSManagedObjectContext!
+    
+    
+    override init() {
+        managedObjectModel = NSManagedObjectModel.mergedModel(from: nil)
+        persistentStoreCoordinator = NSPersistentStoreCoordinator(managedObjectModel: managedObjectModel)
+        let domains = FileManager.SearchPathDomainMask.userDomainMask
+        let directory = FileManager.SearchPathDirectory.documentDirectory
+        let applicationDocumentsDirectory = FileManager.default.urls(for: directory, in: domains).first!
+        let options = [NSMigratePersistentStoresAutomaticallyOption : true, NSInferMappingModelAutomaticallyOption : true]
+        let storeURL = applicationDocumentsDirectory.appendingPathComponent("Product.sqlite")
+        
+        try! persistentStoreCoordinator.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: storeURL, options: options)
+        
+        managedObjectContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+        managedObjectContext.persistentStoreCoordinator = persistentStoreCoordinator
+        managedObjectContext.undoManager = nil
+        
+        super.init()
+        
+    }
+    
+    
 }
