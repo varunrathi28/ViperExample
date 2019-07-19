4d827ad6733c4f24ea178bf3be40d8285295935a
Varun Rathi <varunrathi28@gmail.com>
2019-07-19 21:47:04 +0530

    Core Data Store for CRUD operations

--- ViperStore/Classes/Data Store/CoreDataStore.swift
+++ ViperStore/Classes/Data Store/CoreDataStore.swift
@@ -8,13 +8,17 @@
 
 import UIKit
 import CoreData
+import RxSwift
+
 
 class CoreDataStore:NSObject {
     static let sharedInstance  = CoreDataStore()
     var persistentStoreCoordinator : NSPersistentStoreCoordinator!
     var managedObjectModel : NSManagedObjectModel!
     var managedObjectContext : NSManagedObjectContext!
+    var cartItemsArray: Variable<[Product]> = Variable([])
     
+    //MARK: Initializer
     
     override init() {
         managedObjectModel = NSManagedObjectModel.mergedModel(from: nil)
@@ -32,8 +36,70 @@ class CoreDataStore:NSObject {
         managedObjectContext.undoManager = nil
         
         super.init()
-        
     }
     
+    //MARK: - READ
     
+    func fetchCartItems(_ completionBlock:(([Product])->Void)!){
+        self.fetchEnteriesInPredicate(completionBlock)
+    }
+    
+    func fetchEnteriesInPredicate(_ completionBlock:(([Product])->Void)!) {
+        
+        let fetchRequest : NSFetchRequest<Product> = Product.fetchRequest()
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "productId", ascending: true)]
+        fetchRequest.returnsObjectsAsFaults = false
+        
+        managedObjectContext.perform {
+            let fetchResults = try! self.managedObjectContext.fetch(fetchRequest)
+            completionBlock(fetchResults)
+        }
+    }
+    
+    //MARK: Save
+    
+    
+    func save(){
+    
+        do {
+            try managedObjectContext.save()
+        }
+        catch let error{
+                print("Error Saving Context :\(error)")
+        }
+    }
+    
+    //MARK: Delete
+    func deleteObject(cartItem: Product) {
+        managedObjectContext.delete(cartItem)
+    }
+    
+    func discardMOCChanges(){
+        managedObjectContext.rollback()
+    }
+    
+    //MARK: Insert
+    
+    func newCartItem()->Product {
+        let cartItem = Product(context: managedObjectContext)
+        return cartItem
+    }
+    
+    func checkForSimilarCartItemAndDelete(cartItemToCheck: Product) {
+        for cartItem in self.cartItemsArray.value {
+            if cartItem.productId == cartItemToCheck.productId && cartItemToCheck != cartItem {
+                deleteObject(cartItem: cartItem)
+            }
+        }
+    }
+    
+    func deleteCartItem(withProductId productId: Int16) {
+        for cartItem in self.cartItemsArray.value {
+            if cartItem.productId == productId {
+                deleteObject(cartItem: cartItem)
+                save()
+                break;
+            }
+        }
+    }
 }
